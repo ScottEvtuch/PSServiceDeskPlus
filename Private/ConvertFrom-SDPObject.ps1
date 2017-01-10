83e456bd59b0255eae749db482727c34944819e4
Scott Evtuch <scott@evtuch.net>
2017-01-10 15:23:38 -0600

    Add time conversion to datetime

--- Private/ConvertFrom-SDPObject.ps1
+++ Private/ConvertFrom-SDPObject.ps1
@@ -25,18 +25,42 @@ function ConvertFrom-SDPObject
 
     Process
     {
+        # Set up variables
+        $Epoch = New-Object DateTime 1970,1,1,0,0,0,([DateTimeKind]::Utc)
+
         # Iterate through the results
         $OutputObjects = @()
         foreach ($Object in $InputObject)
         {
             $OutputObject = $Properties.Clone()
 
+            # If a URI was passed to us, generate an ID property from it
             if ($InputObject.URI -ne $null)
             {
                 $Split = $InputObject.URI.Split('/')
                 $OutputObject.Add("$($Split[-3])id",$Split[-2])
             }
-            $InputObject.parameter | % {$OutputObject.Add($_.Name,$_.Value)}
+
+            # Add the parameters to the object properties
+            $InputObject.parameter | % {
+                if ($_.Name -like "*time")
+                {
+                    # Convert the epoch integer to a datetime before adding
+                    if ($_.Value -eq -1)
+                    {
+                        $OutputObject.Add($_.Name,$null)
+                    }
+                    else
+                    {
+                        $OutputObject.Add($_.Name,$Epoch.AddMilliseconds($_.Value).ToLocalTime())
+                    }
+                }
+                else
+                {
+                    # Add normal properties
+                    $OutputObject.Add($_.Name,$_.Value)
+                }
+            }
             $OutputObjects += New-Object -TypeName PSObject -Property $OutputObject
         }
 
